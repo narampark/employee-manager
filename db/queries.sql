11a4f8f2173ac6496470182874c89ac968566cd0
GodSlappedMe <narampark92@gmail.com>
2023-11-20 02:21:45 -0500

    queries.sql

---
+++ db/queries.sql
@@ -0,0 +1,5 @@
+-- SELECT department.department_name AS department, reviews.review
+-- FROM
+-- LEFT JOIN department
+-- ON reviews.department_id = department.IDENTIFIED
+-- ORDER BY departments.department_name;
